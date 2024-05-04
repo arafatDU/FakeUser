f0381c441a35371debd6b1174589da4efd93ad8a
ARAFAT <munshiarafathussain@gmail.com>
2024-05-04 07:37:22 +0600

    Routes and controller setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const router = require('./routes/userRoute');
 
 const app = express();
 const PORT = 3000;
 
 // middleware
 app.use(express.json());
+
+
+// routes
+app.use('/api', router)
 
 app.get('/', (req, res) => {
   res.send('Hello World');
