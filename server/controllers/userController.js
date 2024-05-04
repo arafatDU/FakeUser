8314fdaaaa618bae735abac5dbfc0f097347857f
ARAFAT <munshiarafathussain@gmail.com>
2024-05-04 10:22:15 +0600

    working with faker-js/faker

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,17 +13,38 @@ const userController = (req, res) => {
 
     const errorRateNumber = Math.abs(parseInt(errorRate), 10);
     const seedNumber = Math.abs(parseInt(seed), 10);
-    //const fakers = allfakers[region.toLowerCase()] || faker;
-
+    
     console.log('errorRateNumber', errorRateNumber);
     console.log('seedNumber', seedNumber);
-    //console.log('fakers', fakers);
+    console.log('region', region);
+    
+    faker.seed(seedNumber);
+    // name
+    const firstName = faker.person.firstName()
+    const middleName = faker.person.middleName()
+    const lastName = faker.person.lastName()   
+
+    // address
+    const city = faker.location.city()
+    const street = faker.location.streetAddress()
+    const country = faker.location.country()
+    const zipCode = faker.location.zipCode()
 
     return res.status(200).json({
       message: 'Get all users',
+      data: {
+        firstName,
+        middleName,
+        lastName,
+        city,
+        street,
+        country,
+        zipCode,
+      },
     });
   } catch (error) {
-    returnres.status(500).json({
+    console.error('Error in userController', error);
+    return res.status(500).json({
       message: 'Internal server error',
     });
   }
