529b500b4359c797dfef066dd5694f6bb2b7f6e9
ARAFAT <munshiarafathussain@gmail.com>
2024-05-04 08:23:50 +0600

    settin up faker-js & request query

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,32 @@
+const { allfakers, faker } = require('@faker-js/faker');
+
+
 const userController = (req, res) => {
-  res.send('From user controller');
+  try {
+    if(!req.query) {
+      return res.status(400).json({
+        message: 'Bad request',
+      });
+    }
+
+    const { errorRate, region, seed } = req.query;
+
+    const errorRateNumber = Math.abs(parseInt(errorRate), 10);
+    const seedNumber = Math.abs(parseInt(seed), 10);
+    //const fakers = allfakers[region.toLowerCase()] || faker;
+
+    console.log('errorRateNumber', errorRateNumber);
+    console.log('seedNumber', seedNumber);
+    //console.log('fakers', fakers);
+
+    return res.status(200).json({
+      message: 'Get all users',
+    });
+  } catch (error) {
+    returnres.status(500).json({
+      message: 'Internal server error',
+    });
+  }
 };
 
 
